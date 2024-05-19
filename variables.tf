452a28ad46a1bf26c79dc8ad11e24a9a7047a940
Nikitha2697 <165024387+Nikitha2697@users.noreply.github.com>
2024-05-19 13:36:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,28 +9,3 @@ variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   default     = ""
 }
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-
-variable "ami" {
-   type        = string
-   description = " AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
-}
-
-variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t2.micro"
-}
-
-variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
-}
